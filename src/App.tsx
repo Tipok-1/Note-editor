dc418aceaa8f63a3002f06798fc3de38d7325801
Timofei <tima.chaburko@mail.ru>
2023-07-26 20:30:03 +0300

    feat:connected theme, store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import ControlPanel from './components/ControlPanel/ControlPanel';
+import NoteList from './components/NoteList/NoteList';
+import AddNoteButton from './components/AddNoteButton/AddNoteButton';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const Theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
   return (
-    <div >
-    </div>
+    <ThemeProvider theme={Theme}>
+      <Container maxWidth='md' sx={{
+        height:'100vh',
+        display:'flex',
+        flexDirection:'column'
+      }}>
+        <ControlPanel/>
+        <NoteList></NoteList>
+        <AddNoteButton/>
+      </Container>
+    </ThemeProvider>
   );
 }
 
